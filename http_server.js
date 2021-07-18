a4b99b051b9171b137516b9d2b1bbccc7cb4fbdb
Vipul Jain <33192963+vipul0@users.noreply.github.com>
2021-07-18 09:33:20 +0530

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
-}).listen(3000, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:3000/');
+}).listen(3000, "0.0.0.0");
+console.log('Server running at 3000/');
